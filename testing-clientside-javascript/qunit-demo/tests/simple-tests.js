70c479fcb1afba0e36e8bfe7965454fc8144a6cf
Victor Vilela <vilela_victor@hotmail.com>
2018-03-23 12:47:16 -0500

    add simple tests

---
+++ testing-clientside-javascript/qunit-demo/tests/simple-tests.js
@@ -0,0 +1,11 @@
+QUnit.test( "hello first test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "hello second test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+});
+
+QUnit.test( "hello third test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+});
