1e61c601ba6279a3aaf264b2d6251571c7b7d981
Victor Vilela <vilela_victor@hotmail.com>
2018-03-26 10:27:04 -0500

    add a spec to calculator.divideNumbers and refactor

--- testing-clientside-javascript/jasmine-demo/scripts/helpers/calculator.js
+++ testing-clientside-javascript/jasmine-demo/scripts/helpers/calculator.js
@@ -7,7 +7,7 @@ Calculator.prototype.addNumbers = function(number1, number2){
 };
 
 Calculator.prototype.divideNumbers = function(number1, number2){
-    return number1 + number2;
+    return number1 / number2;
 };
 
 module.exports = Calculator;
--- testing-clientside-javascript/jasmine-demo/spec/helpers/calculator-spec.js
+++ testing-clientside-javascript/jasmine-demo/spec/helpers/calculator-spec.js
@@ -1,9 +1,23 @@
 var Calculator = require('../../scripts/helpers/calculator');
 
 describe('Given a calculator instance',function(){
-    var calc = new Calculator();
+    var calc;
+
+    beforeEach(function(){
+        calc = new Calculator();
+    });
+
+    afterEach(function(){
+        calc = null;
+    });
+
     it('Should return 3 when invokes addNumber method with 1 and 2',function(){
         var result = calc.addNumbers(1,2);
         expect(result).toBe(3);
+    });
+
+    it('Should return 2 when invokes divideNumber method with 6 and 3',function(){
+        var result = calc.divideNumbers(6,3);
+        expect(result).toBe(2);
     });
 });
